5a1289f8385e4a51bdf4afd90765765f0212ebda
Hirotaka-Tambo <tankhdigriotvan@gmail.com>
2025-08-29 13:31:11 +0900

    2025-08-29-13:30/LogOutServletの記述

---
+++ src/main/java/com/example/attendance/controller/LogoutServlet.java
@@ -0,0 +1,32 @@
+package com.example.attendance.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession(false);
+		if(session != null) {
+			session.invalidate();
+		}
+		response.sendRedirect("login.jsp");
+		
+	}
+
+}
