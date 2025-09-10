1c3acf1a382e61e88b00d73a2f2c9c2871b41833
Hirotaka-Tambo <tankhdigriotvan@gmail.com>
2025-09-10 16:21:26 +0900

    2025-09-10-16:21/postgresとの連携の設定修正(idとpass)

--- src/main/java/com/example/attendance/util/DBUtils.java
+++ src/main/java/com/example/attendance/util/DBUtils.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DBUtils{
 	private static final String URL = "jdbc:postgresql://localhost:5432/attendance_db";
-	private static final String USER = "your_username";
-	private static final String PASSWORD = "your_password";
+	private static final String USER = "postgres";
+	private static final String PASSWORD = "postgres";
 	
 	public static Connection getConnection() throws SQLException{
 		return DriverManager.getConnection(URL, USER, PASSWORD);
