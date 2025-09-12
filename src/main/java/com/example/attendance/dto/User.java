1beec859d42074eaf52d1e98fcc372bdbefdcc54
Hirotaka-Tambo <tankhdigriotvan@gmail.com>
2025-09-12 09:45:44 +0900

    2025-09-12-09:44/hashPasswordとsaltの設定追加によるDTOの修正

--- src/main/java/com/example/attendance/dto/User.java
+++ src/main/java/com/example/attendance/dto/User.java
@@ -5,63 +5,70 @@ package com.example.attendance.dto;
  */
 public class User {
 	private String username;
-	private String password;
+	private String passwordHash;
+	private String salt;
 	private String role;
-	private boolean enabled; //New Field
+	private boolean enabled;
 	
 	
-	public User(String username, String password, String role) {
-        this(username, password, role, true); // Default to enabled
-        
+	public User(String username, String passwordHash, String salt, String role, boolean enabled) {
+		super();
+		this.username = username;
+		this.passwordHash = passwordHash;
+		this.salt = salt;
+		this.role = role;
+		this.enabled = enabled;
 	}
-	
-    public User(String username, String password, String role, boolean enabled) {
-        this.username = username;
-        this.password = password;
-        this.role = role;
-        this.enabled = enabled;
-        
-    }
-	
+
+
 	public String getUsername() {
 		return username;
 	}
-	
-	
-	
+
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
-	
-	public String getPassword() {
-		return password;
+
+	public String getPasswordHash() {
+		return passwordHash;
 	}
-	
-	
-	public void setPassword(String password) {
-		this.password = password;
+
+
+	public void setPasswordHash(String passwordHash) {
+		this.passwordHash = passwordHash;
 	}
-	
+
+
+	public String getSalt() {
+		return salt;
+	}
+
+
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+
+
 	public String getRole() {
 		return role;
 	}
-	
+
+
 	public void setRole(String role) {
 		this.role = role;
 	}
 
-	
-	
+
 	public boolean isEnabled() {
 		return enabled;
 	}
-	
-	
+
+
 	public void setEnabled(boolean enabled) {
 		this.enabled = enabled;
-	}
-	
+	} 
 	
 
 }
